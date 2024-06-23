65559e2811905d26e939ed6328d9e448f5ff9284
sathish <me.sathish.git@gmail.com>
2024-06-23 21:55:19 +0530

    add static routing

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);

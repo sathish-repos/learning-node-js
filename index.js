6290c7c76acdd81ca4c424ed002e76d619dac5bf
sathish <me.sathish.git@gmail.com>
2024-06-23 22:12:57 +0530

    get all users

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const path = require("path");
 const port = process.env.PORT || 5000;
 const app = express();
 
+const users = [
+  { id: 1, name: "sathish" },
+  { id: 2, name: "ruban" },
+  { id: 3, name: "saran" },
+  { id: 4, name: "nithsh" },
+];
+
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
 
+// get users
+app.get("/api/users", (req, res) => res.json(users));
+
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port, () => {
-  console.log(`server started on port ${port}`);
-});
+app.listen(port, () => console.log(`server started on port ${port}`));

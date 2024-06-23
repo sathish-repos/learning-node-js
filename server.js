dde4f7164f54507621bb0f9aff17dcd9f8725a79
sathish <me.sathish.git@gmail.com>
2024-06-23 12:35:04 +0530

    add routing with html page and fs module

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+// get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async (req, res) => {
   // res.write("Hello world!");
   // res.end();
 
@@ -24,14 +32,28 @@ const server = http.createServer((req, res) => {
 
   try {
     if (req.method === "GET") {
+      let filepath;
       if (req.url === "/") {
-        res.end("<h1>Hey, Hello world!</h1>");
+        filepath = path.join(__dirname, "public", "pages", "index.html");
+        // res.end("<h1>Hey, Hello world!</h1>");
       } else if (req.url === "/about") {
-        res.end("<h1>About Page</h1>");
+        filepath = path.join(__dirname, "public", "pages", "about.html");
+        // res.end("<h1>About Page</h1>");
       } else {
+        filepath = path.join(
+          __dirname,
+          "public",
+          "pages",
+          "page-not-found.html"
+        );
+
         res.statusCode = 404;
-        res.end("<h1>404 Not Found</h1>");
+        // res.end("<h1>404 Not Found</h1>");
       }
+
+      const data = await fs.readFile(filepath);
+      res.setHeader("Content-Type", "text/html");
+      res.end(data);
     } else {
       throw new Error("method not allowed");
     }

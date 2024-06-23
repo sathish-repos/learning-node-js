0f1fb7cd5bc0d9a9f2d77f93a36d2ed29317eef2
sathish <me.sathish.git@gmail.com>
2024-06-23 10:33:31 +0530

    add routing

--- server.js
+++ server.js
@@ -11,7 +11,34 @@ const server = http.createServer((req, res) => {
   // res.writeHead(404, { "Content-Type": "application/json" });
   // res.write(JSON.stringify({ message: "hi sathish" }));
 
-  res.end("<h1>Hey, Hello world!</h1>");
+  // console.log(req.url);
+  // console.log(req);
+
+  // if (req.url === "/") {
+  //   res.end("<h1>Hey, Hello world!</h1>");
+  // } else if (req.url === "/about") {
+  //   res.end("<h1>About Page</h1>");
+  // } else {
+  //   res.end("<h1>404 Not Found</h1>");
+  // }
+
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.end("<h1>Hey, Hello world!</h1>");
+      } else if (req.url === "/about") {
+        res.end("<h1>About Page</h1>");
+      } else {
+        res.statusCode = 404;
+        res.end("<h1>404 Not Found</h1>");
+      }
+    } else {
+      throw new Error("method not allowed");
+    }
+  } catch (error) {
+    res.statusCode = 500;
+    res.end("<h1>Server error</h1>");
+  }
 });
 
 server.listen(PORT, () => {
